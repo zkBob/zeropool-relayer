26c5165f44970855c04c5807dd084454b0cfbf8c
Leonid Tyurin <lenyatyurin162586@gmail.com>
2022-11-08 20:46:55 +0400

    Update getJob enpoint with new job statuses

--- zp-relayer/endpoints.ts
+++ zp-relayer/endpoints.ts
@@ -11,6 +11,7 @@ import {
   checkSendTransactionErrors,
   checkSendTransactionsErrors,
 } from './validation/validation'
+import { sentTxQueue, SentTxState } from './queue/sentTxQueue'
 
 async function sendTransactions(req: Request, res: Response, next: NextFunction) {
   const errors = checkSendTransactionsErrors(req.body)
@@ -108,22 +109,81 @@ async function getTransactionsV2(req: Request, res: Response, next: NextFunction
 }
 
 async function getJob(req: Request, res: Response) {
-  const jobId = req.params.id
-  const job = await poolTxQueue.getJob(jobId)
-  if (job) {
-    const state = await job.getState()
-    const txHash = job.returnvalue
-    const failedReason = job.failedReason
-    const createdOn = job.timestamp
-    const finishedOn = job.finishedOn
+  enum JobStatus {
+    WAITING = 'waiting',
+    FAILED = 'failed',
+    SENDED = 'sended',
+    REVERTED = 'reverted',
+    COMPLETED = 'completed',
+  }
 
-    res.json({
-      state,
-      txHash,
-      failedReason,
-      createdOn,
-      finishedOn,
-    })
+  interface GetJobResponse {
+    createdOn: number
+    failedReason: null | string
+    finishedOn: null | number
+    state: JobStatus
+    txHash: null | string
+  }
+
+  const jobId = req.params.id
+
+  async function getPoolJobState(jobId: string): Promise<GetJobResponse | null> {
+    const job = await poolTxQueue.getJob(jobId)
+    if (!job) return null
+
+    // Default result object
+    let result: GetJobResponse = {
+      createdOn: job.timestamp,
+      failedReason: null,
+      finishedOn: null,
+      state: JobStatus.WAITING,
+      txHash: null,
+    }
+
+    const poolJobState = await job.getState()
+    if (poolJobState === 'completed') {
+      // Transaction was included in optimistic state, waiting to be mined
+      const sentJobId = job.returnvalue[0][1]
+      const sentJob = await sentTxQueue.getJob(sentJobId)
+      // Should not happen here, but need to verify to be sure
+      if (!sentJob) throw new Error('Sent job not found')
+
+      const sentJobState = await sentJob.getState()
+      if (sentJobState === 'waiting' || sentJobState === 'active' || sentJobState === 'delayed') {
+        // Transaction is in re-send loop
+        const txHash = sentJob.data.prevAttempts.at(-1)?.[0]
+        result.state = JobStatus.SENDED
+        result.txHash = txHash || null
+      } else if (sentJobState === 'completed') {
+        const [txState, txHash] = sentJob.returnvalue
+        if (txState === SentTxState.MINED) {
+          // Transaction mined successfully
+          result.state = JobStatus.COMPLETED
+          result.txHash = txHash
+          result.finishedOn = sentJob.finishedOn || null
+        } else if (txState === SentTxState.REVERT) {
+          // Transaction reverted
+          result.state = JobStatus.REVERTED
+          result.txHash = txHash
+          result.finishedOn = sentJob.finishedOn || null
+        }
+      }
+    } else if (poolJobState === 'failed') {
+      // Either validation or tx sendind failed
+      result.state = JobStatus.FAILED
+      result.failedReason = job.failedReason
+      result.finishedOn = job.finishedOn || null
+    }
+    // Other states mean that transaction is either waiting in queue
+    // or being processed by worker
+    // So, no need to update `result` object
+
+    return result
+  }
+
+  const jobState = await getPoolJobState(jobId)
+  if (jobState) {
+    res.json(jobState)
   } else {
     res.json(`Job ${jobId} not found`)
   }
