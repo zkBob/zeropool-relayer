60bf2f4a41d344862044cd0d0d67fc17532202e1
Leonid Tyurin <lenyatyurin162586@gmail.com>
2023-02-22 10:21:24 +0400

    Add env to enable libjs version check

--- zp-relayer/router.ts
+++ zp-relayer/router.ts
@@ -39,16 +39,18 @@ export function createRouter() {
       logger.info('TraceId', { traceId, path: req.path })
     }
 
-    const libJsVersion = req.headers[HEADER_LIBJS] as string
-    let isValidVersion = false
-    try {
-      isValidVersion = semver.gte(libJsVersion, LIBJS_MIN_VERSION)
-    } catch (e) {
-      logger.warn('Invalid libjs version header', { libJsVersion })
-    }
+    if (config.requireLibJsVersion) {
+      const libJsVersion = req.headers[HEADER_LIBJS] as string
+      let isValidVersion = false
+      try {
+        isValidVersion = semver.gte(libJsVersion, LIBJS_MIN_VERSION)
+      } catch (e) {
+        logger.warn('Invalid libjs version header', { libJsVersion })
+      }
 
-    if (!isValidVersion) {
-      throw new ValidationError([{ path: HEADER_LIBJS, message: `Minimum supported version: ${LIBJS_MIN_VERSION}` }])
+      if (!isValidVersion) {
+        throw new ValidationError([{ path: HEADER_LIBJS, message: `Minimum supported version: ${LIBJS_MIN_VERSION}` }])
+      }
     }
 
     next()
