a662ce31e0ad2f033788de3ef2509c06b077bdea
Alexander Filippov <aleksander.fill@gmail.com>
2024-09-13 17:33:18 +0300

    Fix/doublespend in optimistic (#227)
    * Attempt to fix doublespend in optimistic nullifiers
    
    * Fix offset margin
    
    * Fix logs

--- zp-relayer/state/TxStore.ts
+++ zp-relayer/state/TxStore.ts
@@ -1,37 +1,37 @@
 import { hexToNumber, numberToHexPadded } from '@/utils/helpers';
 import type { Redis } from 'ioredis'
 
-const INDEX_BYTES = 6;
+const TIMESTAMP_BYTES = 6; // enough for another ~8000 years
 
 export class TxStore {
   constructor(public name: string, private redis: Redis) {}
 
-  async add(commitment: string, memo: string, index: number) {
-    await this.redis.hset(this.name, { [commitment]: `${numberToHexPadded(index, INDEX_BYTES)}${memo}` })
+  async add(commitment: string, memo: string, timestamp: number) {
+    await this.redis.hset(this.name, { [commitment]: `${numberToHexPadded(timestamp, TIMESTAMP_BYTES)}${memo}` })
   }
 
   async remove(commitment: string) {
     await this.redis.hdel(this.name, commitment)
   }
 
-  async get(commitment: string): Promise<{memo: string, index: number} | null> {
+  async get(commitment: string): Promise<{memo: string, timestamp: number} | null> {
     const data = await this.redis.hget(this.name, commitment);
 
     return data ? { 
-      memo: data.slice(INDEX_BYTES * 2),
-      index: hexToNumber(data.slice(0, INDEX_BYTES * 2)),
+      memo: data.slice(TIMESTAMP_BYTES * 2),
+      timestamp: hexToNumber(data.slice(0, TIMESTAMP_BYTES * 2)),
     } : null;
   }
 
-  async getAll(): Promise<Record<string, {memo: string, index: number}>> {
+  async getAll(): Promise<Record<string, {memo: string, timestamp: number}>> {
     return this.redis.hgetall(this.name).then(keys => Object.fromEntries(
       Object.entries(keys)
         .map(([commit, data]) => 
           [commit,
           { 
-            memo: data.slice(INDEX_BYTES * 2),
-            index: hexToNumber(data.slice(0, INDEX_BYTES * 2)),
-          }] as [string, {memo: string, index: number}]
+            memo: data.slice(TIMESTAMP_BYTES * 2),
+            timestamp: hexToNumber(data.slice(0, TIMESTAMP_BYTES * 2)),
+          }] as [string, {memo: string, timestamp: number}]
         )
       ));
   }
